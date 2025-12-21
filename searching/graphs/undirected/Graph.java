920e5d3627886fd0e2ecba08b6174d37eec19d0f
Brian Tabone <brian.tabone@gmail.com>
2025-12-21 11:16:11 -0600

    Commit files from local

---
+++ searching/graphs/undirected/Graph.java
@@ -0,0 +1,36 @@
+import edu.princeton.cs.algs4.In;
+
+public class Graph {
+    // Create a V-vertex graph with no edges.
+    public Graph (int V) {
+    
+    }
+
+    // Create a graph from intput stream in
+    public Graph (In in) {
+        
+    }
+	
+	// Number of vertices
+	int V() {
+		return 0;
+	}
+	
+	// Number of edges
+	int E() {
+		return 0;
+	}
+	
+	// Add edge v-w to this graph
+	void addEdge(int v, int w) {
+		
+	}
+	
+	Iterable<Integer> adj(int v) {
+		return null;
+	}
+	
+	public String toString() {
+		return "";
+	}
+}
